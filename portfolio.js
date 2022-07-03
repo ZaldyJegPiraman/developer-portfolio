06a99c1641c270a74a24add9b07e49b530b62811
ZaldyJegPiraman <zaldyjeg.piraman@gmail.com>
2022-07-03 10:20:09 +0800

    published 8

--- portfolio.js
+++ portfolio.js
@@ -4,9 +4,9 @@ export const greetings = {
 	name: "Zaldy Jeg Piraman",
 	title: "Hi all, I'm Zaldy",
 	description:
-		"I'm passionate Software Engineer and Full Stack web developer having an experience of web applications with Asp.Net Web Forms, Asp.Net MVC, Asp.Net Core, Angular, ReactJS, Wordpress and Application Development Like Data Mining, Document / Image Converter, Productivity Tool and OCR Application",
+		"I'm a passionate Software Engineer and Full-Stack web developer having an experience with web applications with Asp.Net Web Forms, Asp.Net MVC, Asp.Net Core, Angular, ReactJS, WordPress, and Application Development Like Data Mining, Document / Image Converter, Productivity Tool, and OCR Application.",
 	resumeLink:
-		"https://www.dropbox.com/s/a4a4dym36cq6v6p/Zaldy%20Resume%20Latest%202022.pdf?dl=0",
+		"https://www.dropbox.com/s/huwfa26zzd3z5px/Zaldy%20Resume%20Latest%202022.pdf?dl=0",
 };
 
 export const openSource = {
@@ -31,14 +31,14 @@ export const skillsSection = {
 			lottieAnimationFile: "/developer-portfolio/lottie/skills/fullstack.json", // Path of Lottie Animation JSON File
 			skills: [
 				emoji(
-					"⚡ Building responsive Web Application using Asp.Net MVC / Core"
+					"⚡ Building responsive Web Applications using Asp.Net MVC / Core"
 				),
-				emoji("⚡ Building responsive static websites using Wordpress"),
+				emoji("⚡ Building responsive static websites using WordPress"),
 				emoji(
 					"⚡ Building RESTful APIs in Asp.Net Core REST Framework"
 				),
 				emoji(
-					"⚡ Building Complex Application using 3rd Party Library and API like DevExpress, HangFire, HTMLAgilityPack, Google API, Microsoft API, Google Charts And Paypal REST API"
+					"⚡ Building Complex applications using 3rd Party Library and APIs like DevExpress, HangFire, HTMLAgilityPack, Google API, Microsoft API, Google Charts, And Paypal REST API"
 				),
 			],
 			softwareSkills: [
@@ -96,9 +96,9 @@ export const skillsSection = {
 			title: "Cloud Infra-Architecture",
 			lottieAnimationFile: "/developer-portfolio/lottie/skills/cloudinfra.json", // Path of Lottie Animation JSON File
 			skills: [
-				emoji("⚡ Experience of working on multiple cloud platforms"),
+				emoji("⚡ Experience in working on multiple cloud platforms"),
 				emoji(
-					"⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases"
+					"⚡ Experience in hosting and maintaining websites on virtual machine instances and integrating databases"
 				),
 				emoji(
 					"⚡ Experience in Github Repository and deployment using Github Actions"
@@ -246,7 +246,7 @@ export const educationInfo = [
 		schoolName: "STI College",
 		subHeader: "Bachelor of Science in Information Technology",
 		duration: "June 2006 - May 2010",
-		desc: "Participated in the development of the following projects for learning purposes.",
+		desc: "Participate in the development of the following projects for learning purposes.",
 		descBullets: [
 			"Timekeeping and Payroll with Biometrics (Thesis Project)",
 			"Baccarat Scoreboard (Freelance Project for Casino Widus)",
@@ -260,7 +260,7 @@ export const experience = [
 		company: "Yempo Inc.",
 		companylogo: "/developer-portfolio/img/icons/common/yempo2.png",
 		date: "Aug 2019 – Sept 2019",
-		desc: "I worked as Junior Developer on a AEL Web App. AEL is a Asset Management platform created in Asp.NET MVC with a unique, robust system offering, to help mange the business better and differentiate AEL from its competitors. I troubleshoot some issue's and built a code that will connect AEL to LOC8 API.",
+		desc: "I worked as Junior Developer on an AEL Web App. AEL is an Asset Management platform created in Asp.NET MVC with a unique, robust system offering to help manage the business better and differentiate AEL from its competitors. I troubleshot some issues and built a code that will connect AEL to LOC8 API.",
 		// descBullets: [
 		// 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
 		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
@@ -271,81 +271,81 @@ export const experience = [
 		company: "Gulf Networks Security Solution",
 		companylogo: "/developer-portfolio/img/icons/common/gulfnetworks.jpg",
 		date: "Feb 2017 - Aug 2018",
-		desc: "Worked as System Administrator and Full Stack Web developer to create a E-Commerce website in wordpress and Online Survey Application in Asp.Net Core that can be use by different customer. While Creating Web Application I also work as System Administrator to maintain server, computers, networks, files, software, configure camera system, and troubleshoot some technical problems in the office.",
+		desc: "Worked as System Administrator and Full Stack Web developer to create an E-Commerce website in WordPress and Online Survey Application in Asp.Net Core that different customers can use. While Creating Web Applications, I also work as System Administrator to maintain servers, computers, networks, files, and software, configure camera systems, and troubleshoot some technical problems in the office.",
 	},
 	{
 		role: "Freelance Programmer",
 		company: "Cybersoft Content Services Inc.",
 		companylogo: "/developer-portfolio/img/icons/common/cyber.png",
 		date: "May 2016 - Jan 2017",
-		desc: "Worked as a Freelance Programmer to create Data Mining Application and Participate on creating CATS Web Application. My part is to create a Web Document Viewer that can view different type of documents like PDF, TIFF, JPG, PNG, Text, WORD, Excel, PowerPoint, DBF and XML files and also allow the cybersoft clients to view status and add comments.",
+		desc: "Worked as a Freelance Programmer to create Data Mining applications and Participated in designing the CATS Web Application. My part is to create a Web Document Viewer that can view different documents like PDF, TIFF, JPG, PNG, Text, WORD, Excel, PowerPoint, DBF, and XML files and also allow the Cybersoft clients to view status and add comments.",
 	},
 	{
 		role: "Software Engineer III",
 		company: "PCM Inc.",
 		companylogo: "/developer-portfolio/img/icons/common/pcm.png",
 		date: "Jul 2015 - May 2016",
-		desc: "Worked as a Software Engineer to maintain, add features and convert to mobile responsive the Opstrack e-Procurement System. The application was created in Asp.Net Web Forms with mix C# and VB code connected in MSSQL Database.",
+		desc: "Worked as a Software Engineer to maintain, add features, and convert the Opstrack e-Procurement System to mobile responsive and was developed in Asp.Net Web Forms with a mix of C# and VB code connected to the MSSQL Database.",
 	},
 	{
 		role: "System Developer",
 		company: "Wellcom Telecom Supermarket",
 		companylogo: "/developer-portfolio/img/icons/common/wellcom.jpg",
 		date: "Nov 2014 - Jul 2015",
-		desc: "Worked as a System Developer to maintain Dashboard Web Application, Create new version of Sales Performance Web Application built in Asp.Net Core, Create Data Collector Web Application that will transfer the data of old ERP System to Microsoft Navision system.",
+		desc: "Worked as a System Developer to maintain Dashboard Web Application, Created a new version of Sales Performance Web Application built in Asp.Net Core, Created Data Collector Web Application that will transfer the data of old ERP System to Microsoft Navision system.",
 	},
 	{
 		role: "System Developer",
 		company: "Cybersoft Content Services Inc.",
 		companylogo: "/developer-portfolio/img/icons/common/cyber.png",
 		date: "Mar 2011 - May 2014",
-		desc: "Worked as a System Developer to create Data Mining Application, OCR Application, Image/Document Converter, Productivity Tool and Maintain some legacy application created visual foxpro, DOS Application",
+		desc: "Worked as a System Developer to create Data Mining Application, OCR Application, Image/Document Converter, Productivity Tool and Maintained some legacy applications made in visual FoxPro, DOS Application.",
 	},
 ];
 
 export const projects = [
 	{
 		name: "Zaldy's E-Commerce Web Application",
-		desc: "Zaldy's E-Commerce is a electronic e-commerce store created in Asp.Net MVC with MSSQL Database hosted in Amazon EC2 Instance",
+		desc: "Is an electronic e-commerce store created in Asp.Net MVC with MSSQL Database hosted in Amazon EC2 Instance",
 		github: "https://github.com/ZaldyJegPiraman/AspMVCECommerce.git",
 		link: "http://zaldysecommerce.cloudns.ph/",
 		video: "https://bit.ly/3OUZ1ht",
 	},
 	{
 		name: "Gulfnetworks CVS Application (Online Survey Application)",
-		desc: "CVS Application is a survey application created in Asp.Net Core hosted in Microsoft Azure. This application have reports, manage customer account and can also be use by multiple clients.",
+		desc: "Is a survey application created in Asp.Net Core hosted in Microsoft Azure. This application has reported, manages customer accounts, and can be used by multiple clients.",
 		image: "https://www.dropbox.com/sh/bmsps6rap3vegqu/AACpQFotshu5uNC1dT6fAS8ia?dl=0",
 	},
 	{
 		name: "Gulfnetworks E-Commerce",
-		desc: "Gulfnetworks Ecommerce is a electronic e-commerce store created in wordpress the customer can search,view product and ask for quotation.",
+		desc: "Is an electronic e-commerce store created in WordPress. The customer can search, view products, and ask for a quotation.",
 		image: "https://www.dropbox.com/sh/0ho9htvvdi5594s/AABgURhsJtFaX0ZvCPIfO6AEa?dl=0",
 	},
 	{
 		name: "Cybersoft CATS Web Application",
-		desc: "Cybersoft CATS is Collaborative Activity Tracking System created in Asp.Net MVC. This application allow customer to view documents completed by cybersoft production team. The client can add comment if the completed documents have issues. ",
+		desc: "Is a Collaborative Activity Tracking System created in Asp.Net MVC. This application allows a customer to view documents completed by the Cybersoft production team, and the client can add a comment if the completed documents have issues.",
 		// github: "https://github.com/1hanzla100",
 		image: "https://www.dropbox.com/sh/zwpyc4vt6s57q8e/AADcCO75d6lckhC7h6tTNh7Za?dl=0",
 	},
 	{
 		name: "Cybersoft Application's",
-		desc: "Cybersoft Document Collector is used to organize and save documents to database from different source like cd's, cloud drives, local drives and OCR Application use to extract text from images",
+		desc: "It is a collection of applications that help classify documents. Cybersoft Document Collector is used to organize and save documents to the database from different sources like CDs, cloud drives, local drives, and OCR applications used to extract text from images.",
 		// github: "https://github.com/1hanzla100",
 		image: "https://www.dropbox.com/sh/pek3rbmizlin452/AACN00kx4AQuP4YBlsmQCFAEa?dl=0",
 	},
 	{
 		name: "PCM Opstrack Application",
-		desc: "Opstrack application is a E-Procurement solution created in Asp.Net Web Forms that provide customer safe purchases and it also integrated to most of ERP Systems",
+		desc: "Is an E-Procurement solution created in Asp.Net Web Forms that provides safe customer purchases and is also integrated into most ERP Systems.",
 		image: "https://www.dropbox.com/sh/nst4sdnuag5jenj/AACy7HidJXR90-JZtd4f2fFJa?dl=0",
 	},
 	{
 		name: "Wellcom Telecom Dashboard Application",
-		desc: "Wellcom Dashboard Application is use to monitor sales by month, year, or by store.",
+		desc: "Is used to monitor sales by month, year, or store.",
 		image: "https://www.dropbox.com/sh/mc9yijt758070we/AADGX2QGT1sNbwonC1kGFTPsa?dl=0",
 	},
 	{
 		name: "Wellcom Telecom Sales Performance Application",
-		desc: "Wellcom Sales Performance Application is use to monitor sales history and sales performance of employees.",
+		desc: "Is used to monitor employees' sales history and sales performance.",
 		image: "https://www.dropbox.com/sh/z0s34dgjlz5zp1j/AACt8h51p_WxcIIPj2IivNnca?dl=0",
 	},
 ];
