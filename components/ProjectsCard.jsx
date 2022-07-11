1d4eae05073e917c4d2ffbe35e23bea7ea1e5905
ZaldyJegPiraman <zaldyjeg.piraman@gmail.com>
2022-07-11 10:57:30 +0800

    published 15

--- components/ProjectsCard.jsx
+++ components/ProjectsCard.jsx
@@ -5,84 +5,93 @@ import { Card, CardBody, Col, Button } from "reactstrap";
 import { Fade } from "react-reveal";
 
 const ProjectsCard = ({ data }) => {
-	return (
-		<Col lg="6">
-			<Fade bottom duration={2000} >
-				<Card className="shadow-lg--hover shadow mt-4">
-					<CardBody>
-						<div className="d-flex px-3">
-							<div className="pl-4">
-								<h3>{data.name}</h3>
-								<p className="description mt-3">{data.desc}</p>
-								{data.github ? (
-									<Button
-										className="btn-icon"
-										color="github"
-										href={data.github}
-										target="_blank"
-										rel="noopener"
-										aria-label="Github"
-									>
-										<span className="btn-inner--icon">
-											<i className="fa fa-github" />
-										</span>
-									</Button>
-								) : null}
-								{data.link ? (
-									<Button
-										className="btn-icon"
-										color="success"
-										href={data.link}
-										target="_blank"
-										rel="noopener" aria-label="Twitter"
-									>
-										<span className="btn-inner--icon">
-											<i className="fa fa-arrow-right mr-2" />
-										</span>
-										<span className="nav-link-inner--text ml-1">
-											App Demo
-										</span>
-									</Button>
-								) : null}
-								{data.video ? (
-									<Button
-										className="btn-icon"
-										color="success"
-										href={data.video}
-										target="_blank"
-										rel="noopener" aria-label="Twitter"
-									>
-										<span className="btn-inner--icon">
-											<i className="fa fa-youtube-play mr-2" />
-										</span>
-										<span className="nav-link-inner--text ml-1">
-											Video Demo
-										</span>
-									</Button>
-								) : null}
-								{data.image ? (
-									<Button
-										className="btn-icon"
-										color="success"
-										href={data.image}
-										target="_blank"
-										rel="noopener" aria-label="Twitter"
-									>
-										<span className="btn-inner--icon">
-											<i className="fa fa-picture-o mr-2" />
-										</span>
-										<span className="nav-link-inner--text ml-1">
-											Screenshot
-										</span>
-									</Button>
-								) : null}
-							</div>
-						</div>
-					</CardBody>
-				</Card>
-			</Fade>
-		</Col>
-	);
+  return (
+    <Col lg="6">
+      <Fade bottom duration={2000}>
+        <Card className="shadow-lg--hover shadow mt-4">
+          <CardBody>
+            <div className="d-flex px-3">
+              <div className="pl-4">
+                <h3>{data.name}</h3>
+                <p className="description mt-3">{data.desc}</p>
+                {data.github ? (
+                  <Button
+                    className="btn-icon"
+                    color="github"
+                    href={data.github}
+                    target="_blank"
+                    rel="noopener"
+                    aria-label="Github"
+                  >
+                    <span className="btn-inner--icon">
+                      <i className="fa fa-github" />
+                    </span>
+                  </Button>
+                ) : null}
+                {data.link ? (
+                  <Button
+                    className="btn-icon"
+                    color="success"
+                    href={data.link}
+                    target="_blank"
+                    rel="noopener"
+                    aria-label="Twitter"
+                  >
+                    <span className="btn-inner--icon">
+                      <i className="fa fa-arrow-right mr-2" />
+                    </span>
+                    <span className="nav-link-inner--text ml-1">App Demo</span>
+                  </Button>
+                ) : null}
+                {data.video ? (
+                  <Button
+                    className="btn-icon button-margin-mobile"
+                    color="success"
+                    href={data.video}
+                    target="_blank"
+                    rel="noopener"
+                    aria-label="Twitter"
+                  >
+                    <span className="btn-inner--icon">
+                      <i className="fa fa-youtube-play mr-2" />
+                    </span>
+                    <span className="nav-link-inner--text ml-1">
+                      Video Demo
+                    </span>
+                  </Button>
+                ) : null}
+                {data.image ? (
+                  <Button
+                    className="btn-icon"
+                    color="success"
+                    href={data.image}
+                    target="_blank"
+                    rel="noopener"
+                    aria-label="Twitter"
+                  >
+                    <span className="btn-inner--icon">
+                      <i className="fa fa-picture-o mr-2" />
+                    </span>
+                    <span className="nav-link-inner--text ml-1">
+                      Screenshot
+                    </span>
+                  </Button>
+                ) : null}
+              </div>
+
+              <style jsx>{`
+                @media only screen and (max-width: 511px) {
+                  .button-margin-mobile {
+                    margin-top: 10px !important;
+                  }
+                }
+              `}</style>
+            </div>
+          </CardBody>
+        </Card>
+      </Fade>
+    </Col>
+  );
 };
 
 export default ProjectsCard;
